8ba5cc1f1caf1a298911c2213e557b91f920786e
SREE NILAY <155079562+sreenilay0909@users.noreply.github.com>
2026-06-13 11:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (65.58%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxProfit(int[] p) {
-        int maxprofit=0;
+        int mp=0;
+        int cp=0;
         for(int i=1;i<p.length;i++){
             if(p[i]>p[i-1]){
-                int cup=p[i]-p[i-1];
-                maxprofit=maxprofit+cup;
+                cp=p[i]-p[i-1];
+                mp=mp+cp;
             }
         }
-        return maxprofit;
+        return mp;
     }
 }
